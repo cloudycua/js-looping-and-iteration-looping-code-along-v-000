aecb7c88d0fb1a3f39f94420bed7ec89ebdbdee1
Christine Cua <cloudycua@gmail.com>
2019-02-09 16:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
 function tailsNeverFails() {
   let tails = 0;
-  while (Math.random() >= 0.5) {
+  while (maybeTrue()) {
     ++tails;
   }
   return `You got ${tails} tails in a row `);
